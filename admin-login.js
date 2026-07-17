da51979964606cca8fd53d58fc18bd912041eb9c
Cumbum auto service <cumbumautoservice@gmail.com>
2026-07-17 10:57:38 +0530

    Add admin login functionality with validation

--- admin-login.js
+++ admin-login.js
@@ -1,1 +1,19 @@
 
+document.getElementById("adminLoginForm").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+let username = document.getElementById("username").value;
+let password = document.getElementById("password").value;
+
+if(username === "admin" && password === "123456"){
+
+window.location.href="admin.html";
+
+}else{
+
+alert("Invalid Username or Password");
+
+}
+
+});
