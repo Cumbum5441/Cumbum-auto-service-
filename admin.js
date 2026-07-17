6787a6d7ba54613f29f5cd3175502f92648a307c
Cumbum auto service <cumbumautoservice@gmail.com>
2026-07-17 10:49:32 +0530

    Initialize driver and booking counts on load

--- admin.js
+++ admin.js
@@ -1,1 +1,9 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+
+  document.getElementById("totalDrivers").textContent = 0;
+  document.getElementById("availableDrivers").textContent = 0;
+  document.getElementById("busyDrivers").textContent = 0;
+  document.getElementById("totalBookings").textContent = 0;
+
+});
